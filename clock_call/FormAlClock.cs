73656f71283fdfe92f89cbf332a29fe7a3fe60c7
Lunin Ivan <162127921+IvanLunin24@users.noreply.github.com>
2024-09-27 19:47:40 +0400

    Добавьте файлы проекта.

---
+++ clock_call/FormAlClock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clock_call
+{
+    public partial class FormAlClock : Form
+    {
+        public FormAlClock()
+        {
+            InitializeComponent();
+        }
+    }
+}
